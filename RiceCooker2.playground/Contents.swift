9733b637b3e02115161da8608f0605c826a029da
takayamasae <bornonthexmaseve@au.com>
2023-07-21 00:08:30 +0900

    異常系１を実装

--- RiceCooker2.playground/Contents.swift
+++ RiceCooker2.playground/Contents.swift
@@ -12,7 +12,7 @@ enum Minute: Int {
 class Alarm {
     var timer: Timer?
     var count: Int = 0
-    var limit: Minute = .whiteRiceMode
+    var limit: Minute?
     
     init() {
         timer = Timer.scheduledTimer(timeInterval: 1,
@@ -24,6 +24,12 @@ class Alarm {
 
     // Timerクラスに設定するメソッドは「@objc」キワードを忘れずに付与する
     @objc func countup() {
+        // nilだったら次に行かない。nilじゃなかったら次に行く。
+        guard let limit = limit else {
+            print("タイマーがセットされていません")
+            timer?.invalidate()
+            return
+        }
         // countの値をインクリメントする
         count += 1
         let minutes = limit.rawValue - count
@@ -38,7 +44,10 @@ class Alarm {
 }
 
 let alarm = Alarm()
-let limit: Minute = .cakeMode
+let limit: Minute? = nil
+//　limitがnilじゃなかったら、{}に入ってから次に行く。nilだったら、{}に入らず、次に行く。
+if let limit = limit {
+    print("\(limit.rawValue)分に設定されました。")
+}
 alarm.limit = limit
-print("\(limit.rawValue)分に設定されました。")
 
